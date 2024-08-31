61814a2f328b2138a6311feb0a9a54070a84dd37
Nikhil Kumar <nikhilgraphicd@gmail.com>
2024-08-31 23:33:19 +0530

    Home endpoint added

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const transactionController = require('../controllers/transactionController.js');
+const express = require("express");
+const transactionController = require("../controllers/transactionController.js");
 
 const router = express.Router();
 
-router.get('/transactions/:address', transactionController.getTransactions);
+router.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr"
+    );
+
+    if (
+      !response.data ||
+      !response.data.ethereum ||
+      !response.data.ethereum.inr
+    ) {
+      throw new Error("Invalid response from CoinGecko API");
+    }
+    return res.json({
+      message: "KoinX Backend Assignment",
+      Etherium: "Rs. " + response.data.ethereum.inr,
+    });
+  } catch (error) {}
+});
+router.get("/transactions/:address", transactionController.getTransactions);
 
 module.exports = router;
-
